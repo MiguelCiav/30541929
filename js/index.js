0b430c3753baa306f307cab73f79907d39e6b9b8
MiguelCiav <miguelciavato@gmail.com>
2025-05-06 18:28:46 -0400

    listado de estudiantes dinámico usando javascript

--- js/index.js
+++ js/index.js
@@ -3,4 +3,19 @@ function loadConfig() {
     document.getElementsByClassName("copyright")[0].innerHTML = config["copyRight"];
     document.getElementsByClassName("greeting")[0].innerHTML = config["saludo"] + ", Miguel Ciavato";
 }
-window.onload = loadConfig
+function loadPersons() {
+  for (perfil of perfiles) {
+    let profile = document.createElement("li");
+    let profileName = document.createElement("a");
+    let profileImg = document.createElement("img");
+    profileName.innerHTML = `${perfil["nombre"]}`;
+    profileImg.setAttribute("src", `${perfil["imagen"]}`);
+    profile.appendChild(profileImg);
+    profile.appendChild(profileName);
+    document.getElementById("persons-list").appendChild(profile);
+  }
+}
+window.onload = function () {
+  loadConfig();
+  loadPersons();
+}
